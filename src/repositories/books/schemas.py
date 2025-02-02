bec45d77f5f2c718e257564e655605e77e9f7101
rhoitjadhav <jadhavrohit497@gmail.com>
2025-02-02 16:22:48 +0530

    update type hint

--- src/repositories/books/schemas.py
+++ src/repositories/books/schemas.py
@@ -1,16 +1,22 @@
+from typing import Optional
+
 from pydantic import BaseModel
+
+from repositories.books.models import CoverType
 
 
 class BooksSchema(BaseModel):
-    title: str
+    book_title: str
     author_id: str
     publisher_id: str
-    books_meta_data: dict
-    media_data: dict
+    books_meta_data: Optional[dict]
+    media_data: Optional[dict]
     pages: int
     publication_year: int
     price: int
     subject: str
+    language: str
+    cover_type: CoverType
 
 
 class ListBooks(BaseModel):
