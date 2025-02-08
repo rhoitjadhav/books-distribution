16580f2a528593d593d686b308958e529159aafc
rhoitjadhav <jadhavrohit497@gmail.com>
2025-02-08 20:08:34 +0530

    added HTTPException

--- src/common/exceptions.py
+++ src/common/exceptions.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from sqlalchemy.exc import NoResultFound
 from starlette.responses import JSONResponse
 
@@ -6,7 +6,10 @@ from starlette.responses import JSONResponse
 async def exception_handler(_: Request, exc: Exception):
     if isinstance(exc, NoResultFound):
         return JSONResponse(status_code=404, content={"error": str(exc)})
-
+    elif isinstance(exc, HTTPException):
+        return JSONResponse(
+            status_code=exc.status_code, content={"error": exc.detail}
+        )
     return JSONResponse(
         status_code=500,
         content={"error": "Something went wrong", "detail": str(exc)},
