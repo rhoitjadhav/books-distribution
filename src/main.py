23c6b42e6175f0a7dcd5b8f0eaa63b6110dcb47f
rhoitjadhav <jadhavrohit497@gmail.com>
2025-03-07 15:24:17 +0530

    added CORS

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 # Packages
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from apis import apis
 from common.exceptions import add_exception_handlers
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Add exception handlers
 add_exception_handlers(app)
