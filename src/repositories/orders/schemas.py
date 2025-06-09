bf6804d0856ae425c34a2731681034b5616c920a
rhoitjadhav <jadhavrohit497@gmail.com>
2025-06-09 16:50:49 +0530

    added OrdersSchema, OrderItemsSchema

--- src/repositories/orders/schemas.py
+++ src/repositories/orders/schemas.py
@@ -1,7 +1,29 @@
+from datetime import datetime
+from uuid import UUID
+
 from common.utils import EnumBase
+from repositories.base import BaseSchema
 
 
 class OrderStatus(EnumBase):
     PENDING = "pending"
     CONFIRMED = "confirmed"
     PROCESSING = "processing"
+
+
+class OrdersSchema(BaseSchema):
+    order_id: str
+    user_id: UUID
+    total_amount: float
+    status: str
+    created_at: datetime = None
+    updated_at: datetime = None
+
+
+class OrderItemsSchema(BaseSchema):
+    order_item_id: UUID
+    order_id: str
+    quantity: int
+    price: float
+    created_at: datetime = None
+    updated_at: datetime = None
