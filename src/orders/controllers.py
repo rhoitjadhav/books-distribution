55f80ca98aa3d6dd1edfc9cc23f57a66c6c2591d
rhoitjadhav <jadhavrohit497@gmail.com>
2025-06-09 16:51:19 +0530

    added get_order()

--- src/orders/controllers.py
+++ src/orders/controllers.py
@@ -8,14 +8,26 @@ from repositories.orders.models import OrdersModel, OrderItemsModel
 router = APIRouter(prefix="/orders")
 
 
-@router.get("")
-def get_orders(
+@router.get("/{order_id}")
+def get_order(
+    order_id: str,
     response: Response,
-    user_id: str = "75e8b351-f612-4eff-8dfe-6544e73a8df4",  # remove this default value
 ):
     return OrdersService(
-        Response(), OrdersModel, OrderItemsModel, CartItemsModel
-    ).get_orders()
+        response, OrdersModel(), OrderItemsModel, CartItemsModel
+    ).get_order(order_id=order_id)
+
+
+@router.get("")
+def list_orders(
+    response: Response,
+    user_id: str = "75e8b351-f612-4eff-8dfe-6544e73a8df4",  # remove this default value
+    page: int = 1,
+    page_size: int = 10,
+):
+    return OrdersService(
+        response, OrdersModel(), OrderItemsModel, CartItemsModel
+    ).list_orders(user_id=user_id, page=page, page_size=page_size)
 
 
 @router.post("/checkout")
