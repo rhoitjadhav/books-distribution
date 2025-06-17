d43cf3d0c476fa43db37449814bea895a6599621
rhoitjadhav <jadhavrohit497@gmail.com>
2025-06-17 17:50:56 +0530

    added ResponseSchema

--- src/common/schemas.py
+++ src/common/schemas.py
@@ -1,5 +1,13 @@
+import humps
 from pydantic import BaseModel
 
 
 class ErrorSchema(BaseModel):
     message: str
+
+
+class ResponseSchema(BaseModel):
+    class Config:
+        extra = "allow"
+        alias_generator = humps.camelize
+        populate_by_name = True
